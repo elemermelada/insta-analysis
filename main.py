0f5ad27d0df4d6ac1e2c8c0489f81b9b5e699717
elemermelada <iiasanmi9@gmail.com>
2022-08-08 19:50:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,56 @@
+## IMPORTS
 from seleniumwire import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+import json
 
+#REQUEST INTERCEPTOR FUNCTION (ADDS CUSTOM HEADERS)
 def req_int(req):
     del req.headers['x-asbd-id']  
-    req.headers['x-asbd-id'] = '198387'  
+    req.headers['x-asbd-id'] = head1  
     del req.headers['x-csrftoken']  
-    req.headers['x-csrftoken'] = 'Mq8zaku3nylpTQfTk9AsrPIARhswomP3'  
+    req.headers['x-csrftoken'] = head2  
     del req.headers['x-ig-app-id']  
-    req.headers['x-ig-app-id'] = '936619743392459'  
+    req.headers['x-ig-app-id'] = head3  
     del req.headers['Referer']  
-    req.headers['x-ig-www-claim'] = 'hmac.AR0RIUxUnDNk-raCJddgxtYop_Lo-Rk1ZcuspO3MCreuCrfd'  
+    req.headers['x-ig-www-claim'] = head4  
 
-def res_int(req,res):
-    print(res.body)
-
-
+## RUNTIME
+#CREATE DRIVER AND INITIALIZE
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://instagram.com')
-input('Login first')
+input('Login and press enter')
 
+#GRAB DESIRED HEADERS OR FALLBACK TO KNOWN HEADERS + SET INTERCEPTOR
+head1 = input('x-asbd-id: ')
+if head1 == '':
+    head1 = '198387'
+head2 = input('x-csrftoken: ')
+if head2 == '':
+    head2 = 'Mq8zaku3nylpTQfTk9AsrPIARhswomP3'
+head3 = input('x-ig-app-id: ')
+if head3 == '':
+    head3 = '936619743392459'
+head4 = input('x-ig-www-claim: ')
+if head4 == '':
+    head4 = 'hmac.AR0RIUxUnDNk-raCJddgxtYop_Lo-Rk1ZcuspO3MCreuCrfd'
+print('Using headers:', [head1,head2,head3,head4])
 driver.request_interceptor = req_int
-driver.response_interceptor = res_int
-driver.get('https://i.instagram.com/api/v1/friendships/4118873852/followers/?count=10&max_id=580&search_surface=follow_list_page')
-#print(driver.page_source)
+
+#MAKE A REQUEST AND GRAB RESPONSE
+friend = '4118873852'
+max_id = 1
+count = 10
+#EXECUTE UNTIL ALL FOLLOWERS ARE GRABBED (LAST RESPONSE HAS LESS 'USERS' THAN COUNT)
+while True:
+    driver.get('https://i.instagram.com/api/v1/friendships/' + friend + '/followers/?count=' + str(count) + '&max_id=' + str(max_id) + '&search_surface=follow_list_page')
+    element = driver.find_element('tag name','pre')
+    response = element.get_attribute('innerHTML')
+    res_json = json.loads(response)
+    print(res_json["users"])
+
+    if res_json["users"] < count-1:
+
+
+    max_id = max_id + count
 
 input("End of code")
